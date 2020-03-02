cca602bcb90554043eb7ff3a1ad4493cc6021155
alexey chernousov <achernousov4@gmail.com>
2020-03-02 13:20:43 +0300

    base build

---
+++ src/components/Game/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Game.css';
+
+export function Game() {
+  return (
+    <div className="Game">
+      <GameScreen />
+      <BottomBar />
+    </div>
+  );
+}
+
